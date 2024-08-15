7a8a703aa17bead427bdc50a5af6276124c0f868
Juan Montano <juan.montano01@gmail.com>
2024-08-15 11:09:32 -0700

    updated cli for  acceptable actions

--- task.py
+++ task.py
@@ -1,8 +1,33 @@
 import sys
+import json
 
 def main():
-    print("Arguments passed:", sys.argv)
+    #grab the command line arguments
+    args = sys.argv[1:]
+    #grab the path to the json file list
+    path_w = 'task_list.json'
+    #if it doesn't exist, create it
+    try: 
+        with open(path_w, mode = 'x') as json_file: 
+            pass 
+    except FileExistsError:
+        pass
+    # list of acceptable actions: 
+    actions = ['add', 'update', 'delete', 'list', 'mark', 'help']
+    # check for user input and compare to accepted actions
+    if args: 
+        print("Argument passed:", args)
+        if args[0] not in actions:
+            print("Invalid request:", args[0], "\nTry:", actions)
+        else: 
+            print("implementing action")
+    else: 
+        print("No argument passed. \nTry:", actions)
+    #with open(path_w) as json_file: 
+      #  l = json.load(json_file)
     
+    #json_dump(data, json_file) to write to file
+        
 if __name__ == "__main__":
     main()
     
