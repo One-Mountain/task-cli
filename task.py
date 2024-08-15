5ba60b64be86f57be9b2cb3f56f52d59222ca5e3
Juan Montano <juan.montano01@gmail.com>
2024-08-15 11:40:47 -0700

    improved json, started add task

--- task.py
+++ task.py
@@ -8,15 +8,21 @@ def main():
     path_w = 'task_list.json'
     #if it doesn't exist, create it
     try: 
-        with open(path_w, mode = 'x') as json_file: 
-            pass 
-    except FileExistsError:
-        pass
+        with open(path_w) as json_file: 
+            l = json.load(json_file) 
+    except:
+        l = {}
+            
     # list of acceptable actions: 
     actions = ['add', 'update', 'delete', 'list', 'mark', 'help']
     # check for user input and compare to accepted actions
     if args: 
-        print("Argument passed:", args)
+        #action was addition: 
+        if args[0] == "add":
+            if not len(args) == 2: 
+                print("Please add task description in quotes")
+            else: 
+                taskadd(l, args[1])
         if args[0] not in actions:
             print("Invalid request:", args[0], "\nTry:", actions)
         else: 
@@ -27,7 +33,17 @@ def main():
       #  l = json.load(json_file)
     
     #json_dump(data, json_file) to write to file
-        
+def taskadd(tasks, desc):
+    if tasks: 
+        print("tasks to do", tasks, desc)
+    else: 
+        print("task added", desc)
+    
+def tasklist(tasks):
+    if tasks: 
+        print(tasks)
+    else: 
+        print('no current todo list, try adding a task')
 if __name__ == "__main__":
     main()
     
