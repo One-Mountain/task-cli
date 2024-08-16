d1e5b419595e119512c7b268858500e40bd7891f
Juan Montano <juan.montano01@gmail.com>
2024-08-16 12:03:27 -0700

    fixed id counter and completed list functionality

--- task.py
+++ task.py
@@ -29,44 +29,75 @@ def main():
                     print("Please add task description in quotes")
                 else: 
                     taskadd(l, args[1])
+            elif args[0] == "list":
+                if len(args) == 1: 
+                    #only the word list
+                    tasklist(l["todo"])
+                elif len(args) == 2:
+                    if args[1] == "done":
+                         tasklist(l["todo"], "done")
+                    elif args[1] == "todo":
+                         tasklist(l["todo"], "todo")
+                    elif args[1] == "in-progress":
+                         tasklist(l["todo"], "in-progress")
+                    else: 
+                        print("Invalid request: ", args)
+                else: 
+                    print("Invalid request:", args)
     else: 
         print("No argument passed. \nTry:", actions)
-
+def id_counter(tasks):
+    li = tasks['todo']
+    if li: 
+        last_task = len(li)-1
+        return li[last_task]["id"] + 1
+    else: 
+        return 1
+    
 def taskadd(tasks, desc):
     todo = tasks["todo"]
     if todo: 
-        id = len(todo) + 1
         dt = datetime.datetime.now(timezone.utc)
         dt = dt.strftime("%m/%d/%Y, %H:%M:%S")
-        t = {"id": id, 
+        t = {"id": id_counter(tasks), 
              "description": desc,
              "status": "todo",
              "createdAt": dt, 
              "updatedAt": dt
              }
+        
+        
         todo.append(t)
         with open(path_w, "w") as json_file: 
             json.dump(tasks, json_file)
-        with open(path_w, "r") as json_file:
-            to_do = json.load(json_file)
-        print(to_do)
     else: 
         dt = datetime.datetime.now(timezone.utc)
         dt = dt.strftime("%m/%d/%Y, %H:%M:%S")
-        t = {"id": 1, 
+        
+        t = {"id": id_counter(tasks), 
              "description": desc,
              "status": "todo",
              "createdAt": dt, 
              "updatedAt": dt
              }
+        
         todo.append(t)
-        print("task added:\n", todo)
         with open(path_w, "w") as json_file:
             json.dump(tasks, json_file)
     
-def tasklist(tasks):
-    if tasks: 
-        print(tasks)
+def tasklist(tasks, status= "all"):
+    if tasks:
+        if status == "all": 
+            for task in tasks:
+                print(task)
+        else:
+            count = 0 
+            for task in tasks:
+                if task["status"] == status:
+                    print(task)
+                    count = count + 1
+            if count == 0:
+                print("No tasks marked with status " + status)
     else: 
         print('no current todo list, try adding a task')
 if __name__ == "__main__":
